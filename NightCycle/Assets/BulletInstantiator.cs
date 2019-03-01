c5ca09dd3e5f27608502c9e373781fddfe6fd0d7
dylanpomeroy <dylan.pomeroy.j@gmail.com>
2019-02-28 20:45:26 -0500

    Setup object pool for bullets

--- NightCycle/Assets/BulletInstantiator.cs
+++ NightCycle/Assets/BulletInstantiator.cs
@@ -7,6 +7,23 @@ public class BulletInstantiator : MonoBehaviour
     public GameObject BulletPrefab;
     public Transform BulletParent;
 
+    public int BulletPoolSize;
+
+    private Queue<GameObject> BulletPool;
+    private Queue<GameObject> ActiveBullets;
+
+    private void Start()
+    {
+        BulletPool = new Queue<GameObject>();
+        ActiveBullets = new Queue<GameObject>();
+
+        for (var i = 0; i < BulletPoolSize; i++)
+        {
+            var newBullet = Instantiate(BulletPrefab);
+            BulletPool.Enqueue(newBullet);
+        }
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -17,8 +34,20 @@ public class BulletInstantiator : MonoBehaviour
 
     public void ShootBullet()
     {
-        var bullet = GameObject.Instantiate(BulletPrefab, BulletParent);
-        bullet.transform.position = transform.position;
-        bullet.transform.rotation = transform.rotation;
+        GameObject newBullet;
+        if (BulletPool.Count == 0)
+        {
+            newBullet = ActiveBullets.Dequeue();
+        }
+        else
+        {
+            newBullet = BulletPool.Dequeue();
+        }
+
+        newBullet.SetActive(true);
+        newBullet.transform.position = transform.position;
+        newBullet.transform.rotation = transform.rotation;
+
+        ActiveBullets.Enqueue(newBullet);
     }
 }
