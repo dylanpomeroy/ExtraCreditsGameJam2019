600b7499cc6e2fa9c86123cfba296ade3dde99f2
dylanpomeroy <dylan.pomeroy.j@gmail.com>
2019-03-01 18:13:09 -0500

    Setup money balance manager

---
+++ NightCycle/Assets/MoneyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyController : MonoBehaviour
+{
+    public int MoneyBalance { get; private set; }
+
+    public void AddMoney(int amount)
+    {
+        MoneyBalance += amount;
+        this.UpdateMoneyBalanceText();
+    }
+
+    public void SubtractMoney(int amount)
+    {
+        MoneyBalance -= amount;
+        this.UpdateMoneyBalanceText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            this.AddMoney(100);
+        }
+    }
+
+    private void UpdateMoneyBalanceText()
+    {
+        GetComponent<Text>().text = $"Money: ${MoneyBalance}";
+    }
+}
