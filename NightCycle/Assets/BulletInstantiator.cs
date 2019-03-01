734f31b0f9bd8f99e05a0fe51df6b5a6166d2dd4
dylanpomeroy <dylan.pomeroy.j@gmail.com>
2019-02-28 20:34:38 -0500

    Setup basic bullet shooting based on rotation of player

---
+++ NightCycle/Assets/BulletInstantiator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletInstantiator : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public Transform BulletParent;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootBullet();
+        }
+    }
+
+    public void ShootBullet()
+    {
+        var bullet = GameObject.Instantiate(BulletPrefab, BulletParent);
+        bullet.transform.position = transform.position;
+        bullet.transform.rotation = transform.rotation;
+    }
+}
---
+++ NightCycle/Assets/LookAtMouse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePosition = Input.mousePosition;
+        mousePosition.z = 5.23f;
+        var objectPosition = Camera.main.WorldToScreenPoint(transform.position);
+        mousePosition.x = mousePosition.x - objectPosition.x;
+        mousePosition.y = mousePosition.y - objectPosition.y;
+        var angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
