65d1f92f613ab8a062fc21b4134bf581d8276670
dylanpomeroy <dylan.pomeroy.j@gmail.com>
2019-02-28 19:59:06 -0500

    Created unity project and added darkness script and ground texture

---
+++ NightCycle/Assets/DarknessController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DarknessController : MonoBehaviour
+{
+    private RawImage image;
+
+    public void MakeDark()
+    {
+        var color = image.color;
+        color.a = 0.7f;
+        image.color = color;
+    }
+
+    public void MakeLight()
+    {
+        var color = image.color;
+        color.a = 0.0f;
+        image.color = color;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+            MakeDark();
+        if (Input.GetKeyDown(KeyCode.M))
+            MakeLight();
+    }
+}
---
+++ NightCycle/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (speed == 0f) speed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+            moveVector += Vector2.up;
+        if (Input.GetKey(KeyCode.S))
+            moveVector += Vector2.down;
+        if (Input.GetKey(KeyCode.A))
+            moveVector += Vector2.left;
+        if (Input.GetKey(KeyCode.D))
+            moveVector += Vector2.right;
+
+        moveVector = moveVector.normalized * speed * Time.deltaTime;
+
+        transform.position += new Vector3(moveVector.x, moveVector.y);
+    }
+}
