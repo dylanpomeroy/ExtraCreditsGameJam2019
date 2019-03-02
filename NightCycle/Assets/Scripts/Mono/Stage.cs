df19404b972e7bee670112b5d9a4dc7bec987993
dylanpomeroy <dylan.pomeroy.j@gmail.com>
2019-03-02 18:53:09 -0500

    refactoring in staging script

--- NightCycle/Assets/Scripts/Mono/Stage.cs
+++ NightCycle/Assets/Scripts/Mono/Stage.cs
@@ -53,7 +53,6 @@ namespace Assets.Scripts
             var currentStep = steps[currentStepIndex];
             if (!currentStep.IsStarted)
             {
-                Debug.Log($"Stage {StageIndex} starting step {currentStepIndex}");
                 currentStep.Start();
             }
             else if (currentStep.IsInProgress)
--- NightCycle/Assets/Scripts/Mono/StageController.cs
+++ NightCycle/Assets/Scripts/Mono/StageController.cs
@@ -19,17 +19,19 @@ public class StageController : MonoBehaviour
 
     private void Start()
     {
-        stages = new List<Stage>();
-
-        var startingStage = new Stage(
+        stages = new List<Stage>
+        {
+            new Stage(
             0,
             new List<StageStep>
             {
                 new StageStep(
                     stepAction: () =>
                     {
-                        GoalText.text = "Goal: Pick up all the coins off the ground";
-                        HintText.text = "Hint: You can move with the WASD keys";
+                        SetTexts(
+                            "Pick up all the coins off the ground",
+                            "You can move with the WASD keys");
+
                         for (var i = 0; i < 19; i++)
                         {
                             var randomVector = new Vector2(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
@@ -42,14 +44,15 @@ public class StageController : MonoBehaviour
                 new StageStep(
                     stepAction: () =>
                     {
-                        GoalText.text = "Goal: Purchase a handgun and some ammo";
-                        HintText.text = "Hint: Click the purchase buttons and then Done when you're finished";
+                        SetTexts(
+                            "Purchase a handgun and x900 ammo",
+                            "Click the Purchase buttons and then Done when you're finished");
+
                         MarketMenu.SetActive(true);
                     },
                     checkCompleted: () => !MarketMenu.activeSelf),
-            });
-
-        var stage1 = new Stage(
+            }),
+            new Stage(
             1,
             new List<StageStep>
             {
@@ -59,8 +62,10 @@ public class StageController : MonoBehaviour
                 new StageStep(
                     stepAction: () =>
                     {
-                        GoalText.text = "Goal: Shoot all the enemies before they get to you!";
-                        HintText.text = "Hint: You can aim with the mouse and shoot with left click";
+                        SetTexts(
+                            "Shoot all the enemies before they get to you!",
+                            "You can aim with the moue and shoot with left click");
+
                         EnemyInstantiator.SpawnEnemies(10);
                     },
                     checkCompleted: () => EnemyInstantiator.ActuallyActiveEnemyCount == 0),
@@ -70,29 +75,43 @@ public class StageController : MonoBehaviour
                 new StageStep(
                     stepAction: () =>
                     {
-                        GoalText.text = "Goal: Finish collecting all the coins dropped by the enemies";
+                        SetTexts(
+                            "Finish collecting all the coints dropped by the enemies");
+
                         HintText.text = string.Empty;
                     },
                     checkCompleted: () => CoinInstantiator.ActuallyActiveCoinCount == 0),
                 new StageStep(
                     stepAction: () =>
                     {
-                        GoalText.text = "Goal: Prepare yoursle for the next attack!";
-                        HintText.text = "Hint: Once you earn enough money you can purchase better weapons";
+                        SetTexts(
+                            "Prepare yourself for the text attack!",
+                            "Once you earn enough money you can purchase better weapons");
+
                         MarketMenu.SetActive(true);
                     },
                     checkCompleted: () => !MarketMenu.activeSelf),
-            });
+            })
+        };
+    }
 
-        stages.Add(startingStage);
-        stages.Add(stage1);
+    private void SetTexts(string goalText = null, string hintText = null)
+    {
+        if (goalText != null)
+        {
+            GoalText.text = $"Goal: {goalText}";
+        }
+
+        if (hintText != null)
+        {
+            HintText.text = $"Hint: {hintText}";
+        }
     }
 
     private void Update()
     {
         if (currentStageIndex == stages.Count)
         {
-            Debug.Log("Game Complete!");
             GoalText.text = "Congratulations, you beat the game!";
             return;
         }
@@ -100,7 +119,6 @@ public class StageController : MonoBehaviour
         var currentStage = stages[currentStageIndex];
         if (!currentStage.IsStarted)
         {
-            Debug.Log($"Starting stage {currentStageIndex}");
             currentStage.Start();
         }
         else if (currentStage.IsInProgress)
